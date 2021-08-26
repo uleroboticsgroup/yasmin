bfd5e7b0c0fa10c78b6640903e1c1e41cf5ade29
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-08-26 16:14:51 +0200

    ' replaced by "
    Former-commit-id: 63c642bbfc6975a63c4e742c3573d8db901e40ac

--- yasmin_viewer/setup.py
+++ yasmin_viewer/setup.py
@@ -2,34 +2,34 @@ from glob import glob
 import os
 from setuptools import setup
 
-package_name = 'yasmin_viewer'
+package_name = "yasmin_viewer"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*_launch.py')),
-        (os.path.join('share', package_name, 'yasmin_viewer_web_client'),
-         glob('yasmin_viewer_web_client/build/*.*')),
-        (os.path.join('share', package_name, 'yasmin_viewer_web_client/static/css'),
-         glob('yasmin_viewer_web_client/build/static/css/*.*')),
-        (os.path.join('share', package_name, 'yasmin_viewer_web_client/static/js'),
-         glob('yasmin_viewer_web_client/build/static/js/*.*')),
+        ("share/ament_index/resource_index/packages",
+            ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name), glob("launch/*_launch.py")),
+        (os.path.join("share", package_name, "yasmin_viewer_web_client"),
+         glob("yasmin_viewer_web_client/build/*.*")),
+        (os.path.join("share", package_name, "yasmin_viewer_web_client/static/css"),
+         glob("yasmin_viewer_web_client/build/static/css/*.*")),
+        (os.path.join("share", package_name, "yasmin_viewer_web_client/static/js"),
+         glob("yasmin_viewer_web_client/build/static/js/*.*")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='miguel',
-    maintainer_email='mgons@unileon.es',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="miguel",
+    maintainer_email="mgons@unileon.es",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'yasmin_viewer_node = yasmin_viewer.yasmin_viewer_node:main',
+        "console_scripts": [
+            "yasmin_viewer_node = yasmin_viewer.yasmin_viewer_node:main",
         ],
     },
 )
