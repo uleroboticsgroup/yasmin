95dd4cfd21b430afdc8497e8d51a69ee2bbf4573
Miguel Ángel González Santamarta <mgons@unileon.es>
2021-10-11 13:30:50 +0200

    yasmin iterfaces fixed

--- yasmin_viewer/include/yasmin_viewer/yasmin_viewer_pub.hpp
+++ yasmin_viewer/include/yasmin_viewer/yasmin_viewer_pub.hpp
@@ -12,7 +12,7 @@
 
 #include "yasmin_interfaces/msg/state.hpp"
 #include "yasmin_interfaces/msg/state_info.hpp"
-#include "yasmin_interfaces/msg/status.hpp"
+#include "yasmin_interfaces/msg/state_machine.hpp"
 #include "yasmin_interfaces/msg/structure.hpp"
 #include "yasmin_interfaces/msg/transition.hpp"
 
@@ -30,11 +30,12 @@ public:
   parse_state_info(std::string name, std::shared_ptr<yasmin::State> state,
                    std::map<std::string, std::string> transitions);
 
-  yasmin_interfaces::msg::Structure
-  parse_states(std::shared_ptr<yasmin::StateMachine> fsm);
+  yasmin_interfaces::msg::Structure parse_states(
+      std::map<std::string, std::shared_ptr<yasmin::State>> states,
+      std::map<std::string, std::map<std::string, std::string>> transitions);
 
-  yasmin_interfaces::msg::Status
-  parse_status(std::shared_ptr<yasmin::StateMachine> fsm);
+  yasmin_interfaces::msg::StateMachine
+  parse_state_machine(std::shared_ptr<yasmin::StateMachine> fsm);
 
 protected:
   void start_publisher(std::string fsm_name,
