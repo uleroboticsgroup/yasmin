c37d3e8e94b614d1d5ce52707c675b17ad1f7b0e
William Freidank <37943506+teapfw@users.noreply.github.com>
2025-01-14 10:10:06 -0500

    add comment docs conform 120 character limit

--- yasmin/yasmin/concurrence.py
+++ yasmin/yasmin/concurrence.py
@@ -8,16 +8,37 @@ from yasmin.blackboard import Blackboard
 
 
 class Concurrence(State):
+    """
+    Child class of a State that runs multiple other states in parallel threads.
+
+    Attributes:
+        _states (List[State]): A list of states that will be run in parallel by this state.
+        _outcome_map (Dict[str, Dict[str, str]]): A dictionary correlating the outcomes of the concurrent states to
+                                                  outcomes of this state.
+        _default_outcome (str): A default outcome in case none of the correlations in _outcome_map are satisfied.
+        _intermediate_outcomes (Dict[str, Optional[str]]): A temporary storage of the parallel states's outcomes.
+        _mutex (Lock): A mutex to ensure thread safety of _intermediate_outcomes. 
+    """
 
     def __init__(self, default_outcome: str, outcome_map: Dict[str, Dict[str, str]], states: List[State]) -> None:
+        """
+        Initializes the Concurrence instance.
+
+        :param outcomes: A set of valid outcomes for this state.
+                         Must contain at least one outcome.
+        :raises ValueError: If either the provided outcomes set or states set are empty.
+        """
 
         outcomes = list(set(outcome_map.keys()) | set([default_outcome]))
 
         super().__init__(outcomes)
 
-        self._states = states
-        self._outcome_map = outcome_map
-        self._default_outcome = default_outcome
+        if not states:
+            raise ValueError("There must be at least one state")
+
+        self._states: List[State] = states
+        self._outcome_map: Dict[str, Dict[str, str]] = outcome_map
+        self._default_outcome: str = default_outcome
 
         self._intermediate_outcomes: Dict[str, Optional[str]] = {}
         for state in self._states:
@@ -26,7 +47,13 @@ class Concurrence(State):
         self._mutex = Lock()
 
     def execute(self, blackboard: Blackboard) -> str:
+        """
+        Executes the parallel behavior.
 
+        :param blackboard: An instance of Blackboard that provides the context for execution.
+        :return: The outcome of the execution as a string.
+        """
+        
         state_threads = []
 
         for s in self._states:
@@ -50,16 +77,26 @@ class Concurrence(State):
             return self._default_outcome
 
         if len(satisfied_outcomes) > 1:
-            yasmin.YASMIN_LOG_WARN(f"More than one satisfied outcome after concurrent state execution; returning the first one.")        
+            yasmin.YASMIN_LOG_WARN(f"More than one satisfied outcome after concurrent state execution.")        
 
         return satisfied_outcomes[0]
 
     def execute_and_save_state(self, state: State, blackboard: Blackboard) -> None:
+        """
+        Executes a state and saves its outcome to the intermediate map.
+
+        :param state: A state to execute.
+        :param blackboard: An instance of Blackboard that provides the context for execution.
+        :return: None
+        """
         outcome = state(blackboard)
         with self._mutex:
             self._intermediate_outcomes[state.__str__()] = outcome
 
     def cancel_state(self) -> None:
+        """
+        Cancels the execution of all states.
+        """
         for state in self._states:
             state.cancel_state()
 
