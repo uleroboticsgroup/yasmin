c69c474549661cc1ceb5c30076d8ab8e4a26703c
Paul Verhoeckx <43780894+PaulVerhoeckx@users.noreply.github.com>
2025-04-23 23:25:49 +0200

    Pass current state instead of start state to transition cb (#54)

--- yasmin/src/yasmin/state_machine.cpp
+++ yasmin/src/yasmin/state_machine.cpp
@@ -348,7 +348,7 @@ StateMachine::execute(std::shared_ptr<blackboard::Blackboard> blackboard) {
       YASMIN_LOG_INFO("State machine transitioning '%s' : '%s' --> '%s'",
                       this->current_state.c_str(), old_outcome.c_str(),
                       outcome.c_str());
-      this->call_transition_cbs(blackboard, this->get_start_state(), outcome,
+      this->call_transition_cbs(blackboard, this->current_state, outcome,
                                 old_outcome);
 
       this->current_state_mutex->lock();
