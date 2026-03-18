2d2e8961adcee0c8f0b4b15381a32754c965f521
Maik Knof <knofm@hs-weingarten.de>
2026-03-18 09:28:03 +0100

    add startup CLI arg for opening XML file

--- yasmin_editor/yasmin_editor/yasmin_editor.py
+++ yasmin_editor/yasmin_editor/yasmin_editor.py
@@ -15,18 +15,47 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import argparse
 import sys
-from PyQt5.QtWidgets import QApplication
-from yasmin_editor.plugins_manager.plugin_manager import PluginManager
+
+from PyQt5.QtWidgets import QApplication, QMessageBox
 from yasmin_editor.editor_gui.yasmin_editor import YasminEditor
+from yasmin_editor.plugins_manager.plugin_manager import PluginManager
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument(
+        "--xml-file",
+        dest="xml_file",
+        default="",
+        help="Path to a YASMIN XML state machine to open at startup.",
+    )
+    args, unknown = parser.parse_known_args()
+    return args, unknown
 
 
 def main() -> int:
+    args, unknown = parse_args()
+
     manager = PluginManager()
     manager.load_all_plugins()
 
-    app = QApplication(sys.argv)
+    qt_argv = [sys.argv[0]] + unknown
+    app = QApplication(qt_argv)
     editor = YasminEditor(manager)
+
+    if args.xml_file:
+        try:
+            editor.xml_manager.load_from_xml(args.xml_file)
+            editor.statusBar().showMessage(f"Opened: {args.xml_file}", 3000)
+        except Exception as e:
+            QMessageBox.critical(
+                editor,
+                "Error",
+                f"Failed to open startup XML file: {str(e)}",
+            )
+
     editor.show()
 
     return app.exec_()
