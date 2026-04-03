2cbbfd100b173691c2852534c44126d831e85141
Michal Sojka <michal.sojka@cvut.cz>
2026-04-03 13:21:01 +0200

    Add missing include (#92)
    When building this in rolling, compilation fails with:
    
        src/yasmin_factory.cpp: In member function 'yasmin::StateMachine::SharedPtr yasmin_factory::YasminFactory::create_sm(tinyxml2::XMLElement*)':
        src/yasmin_factory.cpp:254:41: error: 'get_package_share_directory' is not a member of 'ament_index_cpp'
          254 |         package_path = ament_index_cpp::get_package_share_directory(package);
              |                                         ^~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    Add the missing include file to fix the error.

--- yasmin_factory/src/yasmin_factory.cpp
+++ yasmin_factory/src/yasmin_factory.cpp
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#include "ament_index_cpp/get_package_share_directory.hpp"
 #include <algorithm>
 #include <cctype>
 #include <filesystem>
